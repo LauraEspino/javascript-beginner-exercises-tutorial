44225061965c72808ed75bcea506d0c3f9ec1db0
Laura Espiño <espino.le@gmail.com>
2023-03-31 09:08:39 +0000

    Hasta 12 inclusive

--- exercises/12-How_much_the_wedding_costs/app.js
+++ exercises/12-How_much_the_wedding_costs/app.js
@@ -1,9 +1,20 @@
 let guests = prompt('How many people are coming to your wedding?');
 
 function getPrice(guests){
-    let cost = 0;
+    let cost=0;
     // Your code here
- 
+    if (guests <=50){
+        cost=(cost+4000);
+    }
+    else if(guests<=100){
+        cost=(cost+10000);
+    }
+    else if(guests<=200){
+        cost=(cost+15000);
+    }
+    else{
+        cost=(cost+20000);
+    }
 
     return cost;
 }
