7c0b53287a5f38ebe30c6848df1a503d60cc14e8
Laura Espiño <espino.le@gmail.com>
2023-04-17 17:00:19 +0000

    hasta19

--- .learn/resets/18-While_loop/app.js
+++ .learn/resets/18-While_loop/app.js
@@ -1,12 +1,9 @@
 //fix this function:
 function startCounting() {
-	let counter=101;
-	while (counter <= 100, counter>0) {
+	let counter = 100;
+	while (counter <= 100) {
 		counter--;
 		console.log(counter);
-
-
-	
 	}
 
 	return counter;
--- exercises/17-Create_a_for_loop/app.js
+++ exercises/17-Create_a_for_loop/app.js
@@ -1,4 +1,6 @@
 // Declare and write your function here:
+let standardsMaker="I will write questions if I'm stuck";
 
-
-standardsMaker();
+for (let i=0; i<300;i++){
+    console.log(standardsMaker);
+}
