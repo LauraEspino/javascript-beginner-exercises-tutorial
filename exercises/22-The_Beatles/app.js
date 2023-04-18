82db15cb2fa5d5f74804095458ec59643e4ebc58
Laura Espiño <espino.le@gmail.com>
2023-04-18 18:07:16 +0000

    20

--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,5 +1,11 @@
-
-
-//Your code above ^^^
-
+function sing(){
+    let letter = "";
+    for(let i = 0; i < 11; i++){
+        if(i === 4) letter += 'whisper words of wisdom, ';
+        else if (i === 10) letter += 'there will be an answer, let it be';
+        else letter += 'let it be, ';
+    }
+    return letter;
+}
 console.log(sing());
+//Your code above ^^^
